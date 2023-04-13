f7732762285d7fba26ba9d3d13a2d0390bc2a301
ezakrzewska <ewelinazakrzewska1992@gmail.com>
2023-04-13 20:03:30 +0100

    zadanie dzien 4

--- ChallengeApp/ChallengeApp/Program.cs
+++ ChallengeApp/ChallengeApp/Program.cs
@@ -1,2 +1,22 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+
+using System.ComponentModel.Design;
+
+string name = "Ewelina";
+string sex = "Kobieta";
+int age = 29;
+
+if (sex == "Kobieta" && age < 30)
+{
+    Console.WriteLine("Kobieta ponizej 30 lat");
+}
+else if (name == "Ewelina" && age == 33)
+{
+    Console.WriteLine("Ewelina,lat 33");
+}
+else if (sex != "Kobieta" && (age < 18))
+{
+    Console.WriteLine("niepelnoletni mezczyzna");
+}
+
+
